ee6585d3073be0c40d6b00414d54d4ec5abde3a3
Abhinav Kumar Mishra <abhi.kr.m.0000@gmail.com>
2024-09-24 10:22:27 +0530

    Completed 1.3 cohort

--- week-1/offline-class-1/level-2/05-Object.js
+++ week-1/offline-class-1/level-2/05-Object.js
@@ -1,3 +1,10 @@
+// Example Usage for Object Methods
+const sampleObject = {
+  key1: "value1",
+  key2: "value2",
+  key3: "value3",
+};
+
 // Object Methods Explanation
 function objectMethods(obj) {
   console.log("Original Object:", obj);
@@ -14,17 +21,12 @@ function objectMethods(obj) {
   let hasProp = obj.hasOwnProperty("property");
   console.log("After hasOwnProperty():", hasProp);
 
+  let hasKey1 = obj.hasOwnProperty("key1");
+  console.log("After hasOwnProperty():", hasKey1);
+
+  //enter new entries in object obj
   let newObj = Object.assign({}, obj, { newProperty: "newValue" });
   console.log("After Object.assign():", newObj);
-
-
 }
-
-// Example Usage for Object Methods
-const sampleObject = {
-  key1: "value1",
-  key2: "value2",
-  key3: "value3",
-};
 
 objectMethods(sampleObject);
